f37bcd6c165de31d0030b77efc49b50a1651fde5
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-24 12:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let lastIndex=undefined;
+	for(let i=0;i<s.length;i++){
+		if(s[i]==" "){
+			lastIndex=i;
+			break;
+		}
+	}
+	if(lastIndex=="undefined"){
+		lastIndex=s.length;
+	}
+	return s.substring(0,lastIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
